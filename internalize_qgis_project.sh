e367324cc70522b3d2b195e3a5fda6ddb5b3c1c8
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2016-07-08 09:21:45 +0200

    Add QGIS styles for point, line and polygone layer

--- internalize_qgis_project.sh
+++ internalize_qgis_project.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -e 's/host=localhost port=15432/host=db port=5432/' < qgis/project_ext.qgs > qgis/project.qgs
+sed -e 's/host=localhost port=15432/host=db port=5432/' < qgis/project_ext.qgs | \
+    sed -e 's!v=".*/symbols/\(.*\.svg\)"!v="./symbols/\1"!' > qgis/project.qgs
