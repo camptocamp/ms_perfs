ecc7c949b6a8f326abeed7eff4e2b252432e979a
Yves Jacolin <yves.jacolin@camptocamp.com>
2017-12-12 14:11:40 +0100

    Add information and use absolute path for docker-compose

--- measure.sh
+++ measure.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-COMPOSE="docker-compose -f docker-compose.yml -f docker-compose-perfs.yml"
+COMPOSE="/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose-perfs.yml"
 
 export USERS
 export BASE_URLS
 export WARMUP
 export TIME_PER_TEST
 
+echo "Running perf script at $(date)" 
 ${COMPOSE} stop
 ${COMPOSE} rm -f
 ${COMPOSE} build
 ${COMPOSE} up --abort-on-container-exit || true
 
 mkdir -p archives
+
 ./summary.py --csv --html --prefix archives/summary_
+echo "Finished perf script at $(date)"
